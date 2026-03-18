70e27fbc928d158f5ddf964fa07e8bb8a2646cb2
kartikhatwaliya <kartikhatwaliya@gmail.com>
2026-03-18 11:33:19 +0530

    Add files via upload

---
+++ coding python/coding challenge/Q.1 Discount.py
@@ -0,0 +1,7 @@
+#question-1
+price = float(input("Enter price: "))
+discount = float(input("Enter discount percentage: "))
+
+final_price = price - (price * discount / 100)
+
+print("Final price after discount:", final_price)
